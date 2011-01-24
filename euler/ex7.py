52cb8478407d85ccf8144cdd735386914c549c36
Pedro Rodrigues <medecau@gmail.com>
2011-01-24 17:52:23 +0000

    project euler exercise 7

---
+++ euler/ex7.py
@@ -0,0 +1,17 @@
+'''
+Problem 1
+
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10001st prime number?
+'''
+
+from mathkit import next_prime
+
+counter=0
+num=1
+while counter < 10001:
+    num=next_prime(num)
+    counter+=1
+print num
