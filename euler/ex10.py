5b792e12d5e722e9461bea807b57d14d1194ab39
Pedro Rodrigues <medecau@gmail.com>
2011-01-24 18:32:10 +0000

    project euler exercise 10

---
+++ euler/ex10.py
@@ -0,0 +1,18 @@
+'''
+Problem 1
+
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+'''
+
+from mathkit import next_prime
+
+next=2
+primes=[]
+while next<2000000:
+    primes.append(next)
+    next=next_prime(next)
+
+print sum(primes)
