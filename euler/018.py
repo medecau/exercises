568d6d154df6fe32bec0dbae1aa1f11bb984b6fd
Pedro Rodrigues <medecau@gmail.com>
2014-02-11 16:06:03 +0000

    created some external files

--- euler/018.py
+++ euler/018.py
@@ -32,25 +32,10 @@ Find the maximum total from top to bottom of the triangle below:
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 '''
 
-t='''
-75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23'''
+t=open('018.txt', 'r').read()
 
 
-t=t.split('\n')[1:]
+t=t.split('\n')
 t=[i.split(' ') for i in t]
 t=[[int(j) for j in i] for i in t] # ;) thanks python
 
@@ -59,7 +44,7 @@ for i in range(len(t))[-2::-1]:
         t[i][j]+=max(t[i+1][j:j+2])
 
 print t[0][0]
-        
+
 
 
 
