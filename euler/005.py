be676b068655bd8fcc13015f2d8732a88d09f05b
Pedro Rodrigues <medecau@gmail.com>
2014-02-12 22:07:00 +0000

    a bit more readable

--- euler/005.py
+++ euler/005.py
@@ -7,21 +7,29 @@ Problem 5
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 '''
 
+from itertools import count
+
+
 # this list needs not to include all numbers from 1 to 20
 # if I iterate on steps of 20 starting from 20 it will always be divisible by 20
 # primes need be here for obvious reasons but the lower numbers that are divisors
 # to numbers already in the list need not be included
 # i'ts in reverse order because the higher the number is the least probability of
-# matching 
-divisors=[19,18,17,16,15,14,13,12,11]
-num=0
-did_not_find=True
-while did_not_find:
-    num+=20
-    did_not_find=False
-    for each in divisors:
-        if num%each!=0:
-            did_not_find=True
+# matching
+divisors = [19,18,17,16,15,14,13,12,11]
+
+
+for answer in count(20, 20):
+    found = True
+    for div in divisors:
+        if answer % div != 0:
+            found = False
             break
 
-print num
+    if found:
+        break
+    else:
+        continue
+
+
+print answer
